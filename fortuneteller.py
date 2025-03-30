f3d06be1239933c999e945e8e3fc2e95b9b6c9cc
xlNickl <veysel.eskin007@gmail.com>
2025-03-30 22:38:08 +0300

    Create fortuneteller.py

---
+++ fortuneteller.py
@@ -0,0 +1,21 @@
+# Fortune Teller
+import random
+q = input("Ask me:     ")
+a= random.randint(1, 8)
+#print(a)
+if a == 1:
+  print("Yes, definitely.")
+if a == 2:
+  print("It is decidedly so.")
+if a == 3:
+  print("Without a doubt.")
+if a == 4: 
+  print("Reply hazy, try again!")
+if a == 5: 
+  print("Better not tell you now.")
+if a == 6: 
+  print("My sources say no.")
+if a == 7: 
+  print("Outlook not so good.")
+if a == 8:
+  print("Very doubtful:((")
