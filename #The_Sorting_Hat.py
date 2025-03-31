fa13dfb3732a6fb1463d3961a0cd89060d9ac8ae
xlNickl <veysel.eskin007@gmail.com>
2025-03-31 16:29:19 +0300

    The_Sorting_Hat.py

--- #The_Sorting_Hat.py
+++ #The_Sorting_Hat.py
@@ -12,7 +12,7 @@ if q1 == 1:
 elif q1 == 2:
   H += 1
   S += 1  
-elif q1 >= 3:
+else:
  print("Wrong Input.")
 
 q2 = int(input("Q2) When I'm dead, I want to people to remember me as:\n 1) The Good\n 2) The Great\n 3) The Wise\n 4) The Bold\n Answer: "))
@@ -24,7 +24,7 @@ elif q2 == 3:
   R += 2
 elif q2 == 4:
   G += 2
-elif q2 >= 5:
+else:
  print("Wrong Input.")
 
 q3 = int(input("Q3) Which kinf of instrument most pleases your ear? \n 1) The violin\n 2) The trumpet\n 3) The piano\n 4) The drum\n Answer: "))
@@ -37,16 +37,22 @@ elif q3 == 3:
   R += 4
 elif q3 == 4:
   G += 4
-elif q3 >= 5:
+else:
   print("Wrong Input.") 
 
-print('Gryffindor House Point:' )
-print(G)
-print("\nSlytherin House Point: ")
-print(S)
-print("\nRavenclaw House Point ")
-print(R)
-print("\nHufflepuff House Point")
-print(H)
+print('\nGryffindor House Point:', G)
 
+print("\nSlytherin House Point: ", S)
 
+print("\nRavenclaw House Point: ", R)
+
+print("\nHufflepuff House Point: ", H)
+
+if G >= S and G >= R and G >= H:
+ print("\nYour House is 🦁 Gryffindor!!")
+elif S >= R and S >= H:
+  print("\nYour House is 🐍 Slytherin!")
+elif R >= H:
+  print("\nYour House is 🦅 Ravenclaw!")
+else:
+  print("\nYour House is 🦡 Hufflepuff!")
