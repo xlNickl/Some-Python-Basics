474baf94d2a46e352cc079cbf4039c8d5c292d95
xlNickl <veysel.eskin007@gmail.com>
2025-03-31 20:29:55 +0300

    99_bottles.py

---
+++ 99_bottles.py
@@ -0,0 +1,14 @@
+#99_bottles.py
+## for i in range(5):
+##  print('The square of ' + str(i) + ' is ' + str(i*i))
+  #The square of 0 is 0
+  #The square of 1 is 1
+  #The square of 2 is 4
+  #The square of 3 is 9
+  ##The square of 4 is 16 
+
+#for i in range(5):
+#  print(f'The square of {i} is {i*i}')
+
+for i in range(99, 0, -1):
+  print(f' {i} bottles of beer on the wall\n {i} bottles of beer\n Take one down, pass it around\n')
