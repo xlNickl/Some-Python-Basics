80aa82ab686082e18058c237eeb861dc5df14920
xlNickl <veysel.eskin007@gmail.com>
2025-04-01 22:49:20 +0300

    example_grocery_list.py

---
+++ example_grocery_list.py
@@ -0,0 +1,21 @@
+# Storing grades in a class
+#hw_grade1 = 98
+#hw_grade2 = 87
+#hw_grade3 = 92
+#hw_grade4 = 96
+
+#quiz_grade1 = 9
+#quiz_grade2 = 6
+#quiz_grade3 = 8 
+
+# Storing grades in a class
+
+#hw_grades = [98, 87, 92, 96]
+#quiz_grades = [9, 6, 8]
+
+#list_name = [item1, item2, item3, item4]
+
+# grocery_list.py
+
+grocery = ['🥚 Eggs', '🥑 Avocados', '🍪 Cookies', '🌶 Hot Pepper Jam','🫐 Blueberries', '🥦 Broccoli']
+print (grocery)
