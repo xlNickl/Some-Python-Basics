a4f3f5c84f5855c3daa39db11f343fd0764c8c8f
xlNickl <veysel.eskin007@gmail.com>
2025-04-01 23:06:42 +0300

    Create example_to_do_list.py

---
+++ example_to_do_list.py
@@ -0,0 +1,35 @@
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# Index:   0    1    2    3    4
+
+# print(vowels[0])     # Output: a
+# print(vowels[1])     # Output: e
+# print(vowels[2])     # Output: i
+# print(vowels[3])     # Output: o
+# print(vowels[4])     # Output: u
+
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# Index:   0    1    2    3    4
+# Index:  -5   -4   -3   -2   -1
+
+# print(vowels[0 : 3])
+# print(vowels[1 : 3])
+
+# Output:
+# ['a', 'e', 'i']
+# ['e', 'i']
+
+# to_do_list.py
+
+to_do = ['🏦 Get quarters.',
+'🧺 Do laundry.', 
+'🌳 Take a walk.', 
+'💈 Get a haircut.',
+'🍵 Make some tea.',
+'💻 Complete Lists chapter.',
+'💖 Call mom.',
+'📺 Watch My Hero Academia.']
+
+print(to_do[0])
+print(to_do[1])
+print(to_do[2 : 5])
+print(to_do[9])
