c77b8f4b3ab446399ae8243654eda8d804305836
xlNickl <veysel.eskin007@gmail.com>
2025-04-03 23:46:03 +0300

    mixtape.py

---
+++ mixtape.py
@@ -0,0 +1,14 @@
+# mixtape.py
+
+playlist = ['Porches - Rangerover',
+'Mount Eerie - You Swan, Go On',
+'Carolyn Polachek - Look at Me Now',
+'Pinegrove - Darkness',
+'LVL UP - Spirit Was',
+'Mitski - First Love / Late Spring'
+]
+#for i in range(len(playlist)):
+#  print(playlist[i])
+
+for song in playlist:
+  print(song)
